304e82af9851820442c921d170f8086b0bdcc1d8
Kevin Paul Herbert <kph@platinasystems.com>
2019-08-22 22:12:39 -0700

    gpio: Kill dead code and simplify API
    Remove dead code. Don't bother with c-ish bit fields, keep the defaults
    as strings to be more obvious and Go-like. Improve error handling.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- gpio.go
+++ gpio.go
@@ -8,31 +8,18 @@ package gpio
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
-	"sync"
-	"syscall"
-	"time"
+	"strconv"
 )
 
-const (
-	// Lower 16 bits gives index.
-	PinIndexMask Pin = 0xffff
-	// High bits are flags: direction input/output
-	IsInput    Pin = 1 << 31
-	IsOutputLo Pin = 1 << 30
-	IsOutputHi Pin = 1 << 29
-)
-
-type Pin uint32
-
-func (p Pin) Index() int { return int(p & PinIndexMask) }
+type Pin struct {
+	Gpio    int
+	Name    string
+	Default string
+}
 
 type GpioAliasMap map[string]string
-type PinMap map[string]Pin
+type PinMap map[string]*Pin
 
 type Chip struct {
 	// Chip has GPIOs base through base + count.
@@ -49,7 +36,7 @@ var prefix string
 
 func SetDebugPrefix(p string) { prefix = p }
 
-var GpioBankToBase = map[string]Pin{
+var GpioBankToBase = map[string]int{
 	"gpio0": 0,
 	"gpio1": 32,
 	"gpio2": 64,
@@ -59,61 +46,47 @@ var GpioBankToBase = map[string]Pin{
 	"gpio6": 192,
 }
 
-var GpioPinMode = map[string]Pin{
-	"output-high": IsOutputHi,
-	"output-low":  IsOutputLo,
-	"input":       IsInput,
+var GpioPinMode = map[string]string{
+	"output-high": "high",
+	"output-low":  "low",
+	"input":       "in",
 }
 
-func exportPin(p Pin) (err error) {
+func (p *Pin) Export() (err error) {
 	fn := prefix + "/sys/class/gpio/export"
 	f, err := os.OpenFile(fn, os.O_WRONLY, 0)
 	if err != nil {
 		return
 	}
 	defer f.Close()
-	fmt.Fprintf(f, "%d\n", p&PinIndexMask)
+	fmt.Fprintf(f, "%d\n", p.Gpio)
 	return
 }
 
-func pinOpen(p Pin, name string) (f *os.File, fn string, err error) {
-	fn = fmt.Sprintf(prefix+"/sys/class/gpio/gpio%d/%s", p&PinIndexMask, name)
-	f, err = os.OpenFile(fn, os.O_RDWR, 0)
-	if e, ok := err.(*os.PathError); ok && e.Err == syscall.ENOENT {
-		err = exportPin(p)
-		if err != nil {
-			return
-		}
-		// To be safe wait a bit for kernel to create /sys/class/gpio/PIN directory.
-		// need this?
-		time.Sleep(10 * time.Millisecond)
-		f, err = os.OpenFile(fn, os.O_RDWR, 0)
-		if err != nil {
-			return
-		}
+func (p *Pin) IsExported() (x bool) {
+	fn := fmt.Sprintf(prefix+"/sys/class/gpio/gpio%d/value", p.Gpio)
+	_, err := os.Stat(fn)
+	if err != nil {
+		return false
 	}
+	return true
+}
+
+func (p *Pin) Open(name string) (f *os.File, fn string, err error) {
+	fn = fmt.Sprintf(prefix+"/sys/class/gpio/gpio%d/%s", p.Gpio, name)
+	f, err = os.OpenFile(fn, os.O_RDWR, 0)
 	return
 }
 
-func (p Pin) Direction() (isOut bool, err error) {
-	f, fn, err := pinOpen(p, "direction")
+func (p *Pin) Direction() (dir string, err error) {
+	f, _, err := p.Open("direction")
 	if err != nil {
 		return
 	}
 	defer f.Close()
 
-	var v string
+	_, err = fmt.Fscanf(f, "%s\n", &dir)
 
-	fmt.Fscanf(f, "%s\n", &v)
-	switch v {
-	case "out":
-		isOut = true
-	case "in":
-
-	default:
-		err = fmt.Errorf("%s: read unexpected value `%s'", fn, v)
-		return
-	}
 	return
 }
 
@@ -122,151 +95,60 @@ func (p Pin) Direction() (isOut bool, err error) {
 // 	initializing the value as low. To ensure glitch free
 // 	operation, values "low" and "high" may be written to
 // 	configure the GPIO as an output with that initial value.
-func (p Pin) SetDirection() (err error) {
-	d, err := p.Direction()
-	if err != nil {
-		return
-	}
-
-	if d {
-		if (p & (IsOutputLo | IsOutputHi)) != 0 {
-			return
-		}
-	} else {
-		if (p & (IsOutputLo | IsOutputHi)) == 0 {
-			return
-		}
-	}
-	f, _, err := pinOpen(p, "direction")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	v := "in"
-	if p&IsOutputLo != 0 {
-		v = "low"
-	}
-	if p&IsOutputHi != 0 {
-		v = "high"
-	}
-	fmt.Fprintf(f, "%s\n", v)
-	return err
-}
-
-// "value" ... reads as either 0 (low) or 1 (high). If the GPIO
-// 	is configured as an output, this value may be written;
-// 	any nonzero value is treated as high.
-func (p Pin) setGetValue(setValue ...bool) (value bool, err error) {
-	var (
-		f  *os.File
-		fn string
-	)
-	f, fn, err = pinOpen(p, "value")
+func (p *Pin) SetDirection(dir string) (err error) {
+	f, _, err := p.Open("direction")
 	if err != nil {
 		return
 	}
 	defer f.Close()
 
-	var v int
-	if len(setValue) > 0 {
-		if setValue[0] {
-			v = 1
-		}
-		fmt.Fprintf(f, "%d\n", v)
-	} else {
-		_, err = fmt.Fscanf(f, "%d\n", &v)
-		if err != nil {
-			err = fmt.Errorf("%s: parse error %s", fn, err)
-			return
-		}
-	}
-	value = v != 0
+	_, err = fmt.Fprintf(f, "%s\n", dir)
 	return
 }
 
-func (p Pin) SetValue(v bool) (err error) {
-	_, err = p.setGetValue(v)
-	return
-}
-
-func (p Pin) Value() (value bool, err error) {
-	value, err = p.setGetValue()
-	return
-}
-
-func pathScanf(path, format string, args ...interface{}) (n int, err error) {
-	f, err := os.OpenFile(path, os.O_RDONLY, 0)
+func (p *Pin) SetValue(v bool) (err error) {
+	f, _, err := p.Open("value")
 	if err != nil {
 		return
 	}
 	defer f.Close()
-	return fmt.Fscanf(f, format, args...)
-}
-
-var (
-	chips []Chip
-	once  sync.Once
-)
-
-func ChipsMatching(pattern string) ([]Chip, error) {
-	var (
-		err error
-		cs  []Chip
-	)
-
-	once.Do(func() { chips, err = getChips() })
-
-	if len(pattern) == 0 {
-		cs = chips
-	} else {
-		re, err := regexp.Compile(pattern)
-		if err != nil {
-			return cs, err
-		}
-		for i := range chips {
-			var ok bool
-			if _, ok = chips[i].Compatible[pattern]; ok {
-				cs = append(cs, chips[i])
-			}
-			for k := range chips[i].Compatible {
-				if re.MatchString(k) {
-					cs = append(cs, chips[i])
-					break
-				}
-			}
-		}
+	x := 0
+	if v {
+		x = 1
 	}
-	return cs, err
+	_, err = fmt.Fprintf(f, "%d\n", x)
+	return
 }
 
-func getChips() (chips []Chip, err error) {
-	dirPath := prefix + "/sys/class/gpio"
-	fis, err := ioutil.ReadDir(dirPath)
+func (p *Pin) Value() (v bool, err error) {
+	f, _, err := p.Open("value")
 	if err != nil {
 		return
 	}
-	for _, fi := range fis {
-		if fi.IsDir() {
-			var c Chip
-			n := fi.Name()
-			if _, e := fmt.Sscanf(n, "gpiochip%d", &c.Base); e != nil {
-				continue
-			}
-			if _, err = pathScanf(filepath.Join(dirPath, n, "ngpio"), "%d", &c.Count); err != nil {
-				return
-			}
-			var b []byte
-			if b, err = ioutil.ReadFile(filepath.Join(dirPath, n, "device/of_node/compatible")); err != nil {
-				return
-			}
-			cs := strings.Split(string(b), "\x00")
-			c.Compatible = make(map[string]bool)
-			for i := range cs {
-				c.Compatible[cs[i]] = true
-			}
-			chips = append(chips, c)
-		}
+	defer f.Close()
+	x := 0
+	_, err = fmt.Fscanf(f, "%d\n", &x)
+	if x != 0 {
+		v = true
 	}
 	return
+}
+
+func (p *Pin) String() string {
+	return fmt.Sprintf("Gpio: %d (%s)", p.Gpio, p.Name)
+}
+
+func (p *Pin) SetDefault() (err error) {
+	return p.SetDirection(p.Default)
+}
+
+func NewPin(name, mode, bank, index string) (err error) {
+	i, _ := strconv.Atoi(index)
+	p := &Pin{Gpio: GpioBankToBase[bank] + i, Name: name,
+		Default: GpioPinMode[mode]}
+	Pins[name] = p
+	if p.IsExported() {
+		return
+	}
+	return p.Export()
 }
